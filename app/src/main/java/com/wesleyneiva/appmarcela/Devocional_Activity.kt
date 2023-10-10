46bdc6cee3a799b29d47f3d2b04dfcd0f186e292
Wesley <yelsew84@gmail.com>
2023-10-10 17:05:18 -0300

    Criada a activity devocional

---
+++ app/src/main/java/com/wesleyneiva/appmarcela/Devocional_Activity.kt
@@ -0,0 +1,11 @@
+package com.wesleyneiva.appmarcela
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Devocional_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_devocional)
+    }
+}
--- app/src/main/java/com/wesleyneiva/appmarcela/MainActivity.kt
+++ app/src/main/java/com/wesleyneiva/appmarcela/MainActivity.kt
@@ -7,19 +7,31 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var btnAcessar: Button
+    lateinit var btnExercicios: Button
+    //lateinit var btnDevocional: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnAcessar = findViewById(R.id.btn_exercicios)
-        btnAcessar.setOnClickListener {
+        btnExercicios = findViewById(R.id.btn_exercicios)
+        btnExercicios.setOnClickListener {
             val intent = Intent(
-                this, Exercicois_Activity::class.java)
-                startActivity(intent)
+                this, Exercicois_Activity::class.java
+            )
+
 
         }
+        /*btnDevocional = findViewById(R.id.btn_devocional)
+        btnDevocional.setOnClickListener {
+            val intent1 = Intent(
+                this, Devocional_Activity::class.java)
+
+        }*/
+
+        startActivity(intent)
+
+
     }
 }
